f951650a3e6d47e731d5503c1b0b941eca89df8e
Kamil Parczewski <kamil.parczewski@htdevelopers.com>
2018-06-02 16:06:53 +0200

    third task

--- bank/js/page1.js
+++ bank/js/page1.js
@@ -1,5 +1,5 @@
-app.controller("Page1", [ "$http", "globals",
-  function($http, globals) {
+app.controller("Page1", [ "$http", "globals", "$websocket",
+  function($http, globals, $websocket) {
     var ctrl = this;
     ctrl.balance = 0;
     ctrl.limit = 0;
@@ -9,10 +9,16 @@ app.controller("Page1", [ "$http", "globals",
     ctrl.prevrecs = [];
     ctrl.prevrec = '';
 
-    $http.get("/account").then(function(rep) {
-      ctrl.balance = rep.data.balance;
-      ctrl.limit = rep.data.limit;
-    });
+    ctrl.getAccounts = function() {
+      $http.get("/account").then(
+        function(rep) {
+          ctrl.balance = rep.data.balance;
+          ctrl.limit = rep.data.limit;
+        },
+        function(err) {
+          alert(err);
+        });
+    }
     ctrl.getPrevrec = function() {
       $http.get('/prevrec/').then(
         function(rep){
@@ -45,7 +51,20 @@ app.controller("Page1", [ "$http", "globals",
       );
     }
 
+    ctrl.getAccounts();
     ctrl.getPrevrec();
+
+    ctrl.dataStream = $websocket('ws://' + window.location.host);
+
+    ctrl.dataStream.onMessage(function(msg) {
+      try {
+        var data = JSON.parse(msg.data);
+        if(data.type === 'success') {
+          ctrl.getAccounts();
+          ctrl.getPrevrec();
+        }
+      } catch(err) {}
+    });
 
   }
 ]);
